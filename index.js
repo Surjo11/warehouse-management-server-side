9961da39a0562d10bbb8929500b9d50e3741a5fa
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-05-16 03:36:10 +0600

    nodemon added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
