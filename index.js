751eb4b9cf5be54a3a5cf4492e7a41d257720b4d
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-05-16 01:24:45 +0600

    verifyToken

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.port || 5000;
@@ -21,25 +22,44 @@ async function run() {
   try {
     await client.connect();
     const itemCollection = client.db("elsolWarehouse").collection("items");
+
     // Data get from server
     app.get("/items", async (req, res) => {
       let query;
       if (req.query.email) {
+        const tokenInfo = req.headers.authorization;
+        console.log(tokenInfo);
+        const decoded = verifyToken(tokenInfo);
         const email = req.query.email;
-        query = { supplieremail: email };
+        // console.log(email, decoded.email);
+        if (email === decoded.email) {
+          query = { supplieremail: email };
+          const cursor = await itemCollection.find(query);
+          const result = await cursor.toArray();
+          res.send(result);
+        } else {
+          res.send({ message: "Unauthorize access" });
+        }
       } else {
         query = {};
+        const cursor = itemCollection.find(query);
+        const items = await cursor.toArray();
+        res.send(items);
       }
-      const cursor = itemCollection.find(query);
-      const items = await cursor.toArray();
-      res.send(items);
     });
+
     // single data load by id
     app.get("/item/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const item = await itemCollection.findOne(query);
       res.send(item);
+    });
+
+    app.post("/signin", (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+      res.send({ token });
     });
 
     // Data add in server
@@ -82,3 +102,19 @@ run().catch(console.dir);
 app.listen(port, () => {
   console.log("Listening to port", port);
 });
+
+// verify token function
+function verifyToken(token) {
+  let email;
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      email = "Invalid email";
+      // console.log(email);
+    }
+    if (decoded) {
+      email = decoded;
+      // console.log(email);
+    }
+  });
+  return email;
+}
